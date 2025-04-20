1434074f41b393c7cbd3c5c1746a0e67bbae7d5d
Pranathi Poojary <pranathi.poojary20@gmail.com>
2025-04-20 17:52:48 +0100

    [IMP] Sending batches 6 and 7 to run

--- configs/test_configs.py
+++ configs/test_configs.py
@@ -10,7 +10,7 @@ sys.path.append(root_dir)
 errored_out = dict()
 
 def run_tests():
-    dir = "/Users/pranathipoojary/Imperial/FYP/Brain-Network/configs/batch_5"
+    dir = "/Users/pranathipoojary/Imperial/FYP/Brain-Network/configs/batch_7"
     
     # pts = [
     #     "/Users/pranathipoojary/Imperial/FYP/Brain-Network/configs/wildcard/config_zoom_m35.json"
