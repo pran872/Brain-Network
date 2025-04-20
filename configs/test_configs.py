5c8e5ec0ec43f9258ed3fe2c2bc86981e2219d4f
Pranathi Poojary <pranathi.poojary20@gmail.com>
2025-04-20 16:26:58 +0100

    [IMP] Wildcard 35

--- configs/test_configs.py
+++ configs/test_configs.py
@@ -13,15 +13,14 @@ def run_tests():
     dir = "/Users/pranathipoojary/Imperial/FYP/Brain-Network/configs/batch_3"
     
     pts = [
-        "/Users/pranathipoojary/Imperial/FYP/Brain-Network/configs/batch_3/config_resnet_m31.json",
-        "/Users/pranathipoojary/Imperial/FYP/Brain-Network/configs/batch_3/config_resnet_m32.json"
+        "/Users/pranathipoojary/Imperial/FYP/Brain-Network/configs/wildcard/config_zoom_m35.json"
     ]
     # for config in glob.glob(os.path.join(dir, "**", "*.json"), recursive=True):
     for config in pts:
         print(f"\nTesting config: {config}")
         try:
             subprocess.run(
-                ["python", "source/simple_cnn.py", "--config", config],
+                ["python", "source/simple_cnn.py", "--config", config, "-d"],
                 capture_output=True,
                 text=True
             )
