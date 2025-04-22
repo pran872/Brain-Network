5f33882e7e487b1cf7728e8ffc612dbed5e2c098
Pranathi Poojary <pranathi.poojary20@gmail.com>
2025-04-22 15:12:24 +0100

    [IMP] Added more determinism

--- source/simple_cnn.py
+++ source/simple_cnn.py
@@ -1,4 +1,6 @@
 '''Simple CNN'''
+import os
+os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
 import torch
 import torch.nn as nn
 import torchvision
@@ -26,7 +28,6 @@ except ModuleNotFoundError:
 
 from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
-import os
 import logging
 import sys
 import socket
@@ -60,7 +61,7 @@ def set_seed(seed=42):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    # torch.use_deterministic_algorithms(True)
+    torch.use_deterministic_algorithms(True)
 
     return torch.Generator().manual_seed(seed)
 
