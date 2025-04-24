fa31ffa86c9d8a6965f0c06d7e3f3c8bcd98026a
Pranathi Poojary <psp20@login-b.cx3.hpc.ic.ac.uk>
2025-04-24 17:20:54 +0100

    [IMP] Ran p4

--- source/simple_cnn.py
+++ source/simple_cnn.py
@@ -47,7 +47,7 @@ def set_seed(seed=42):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    torch.use_deterministic_algorithms(True)
+    torch.use_deterministic_algorithms(True, warn_only=True)
 
     return torch.Generator().manual_seed(seed)
 
