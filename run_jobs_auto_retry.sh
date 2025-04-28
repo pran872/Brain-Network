4725968a5a8a35211692fcdf9b1c587876840454
Pranathi Poojary <psp20@login-b.cx3.hpc.ic.ac.uk>
2025-04-28 21:00:39 +0100

    [IMP] Ran batch 7v2

--- run_jobs_auto_retry.sh
+++ run_jobs_auto_retry.sh
@@ -8,14 +8,21 @@
 
 cd $PBS_O_WORKDIR
 mkdir -p job_logs
-mv job_logs/train.out job_logs/train_${PBS_JOBID}.out
-mv job_logs/train.err job_logs/train_${PBS_JOBID}.err
+
+CONFIG=${CONFIG:-configs/config_template.json}
+CONFIG_NAME=$(basename "$CONFIG" .json)
+
+cleanup(){
+	echo "Clean up"
+	mv job_logs/train.out job_logs/train_${PBS_JOBID}_${CONFIG_NAME}.out
+	mv job_logs/train.err job_logs/train_${PBS_JOBID}_${CONFIG_NAME}.err
+}
+trap cleanup EXIT
 
 eval "$($HOME/miniforge3/bin/conda shell.bash hook)"
 conda activate brain-network-env
 
-export OUTPUT_DIR=/rds/general/user/psp20/home/Brain-Network/runs/stanford_dogs
-CONFIG=${CONFIG:-configs/config_template.json}
+export OUTPUT_DIR=/rds/general/user/psp20/home/Brain-Network/runs/stanford_dogs/batch_7
 TRAIN_CMD="python source/simple_cnn.py -c $CONFIG"
 
 MAX_RETRIES=3
